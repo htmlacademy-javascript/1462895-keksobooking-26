42c045b98ae3c733fa3537be1026eefafe1c2328
Dima Os <dimaos@protonmail.com>
2022-06-02 13:41:19 +0300

    Добавляет функцию getRandomFloatNumber

--- js/main.js
+++ js/main.js
@@ -8,5 +8,16 @@ const getRandomIntegerNumber = (from = 1, to = 10) => {
   return Math.floor(Math.random() * (to - from + 1)) + from;
 };
 
+const getRandomFloatNumber = (from = 0, to = 10, precision = 5) => {
+  from = from < 0 ? 0 : from;
+
+  if (from > to) {
+    return 0;
+  }
+
+  return (Math.random() * (to - from) + from).toFixed(precision);
+};
+
 getRandomIntegerNumber();
+getRandomFloatNumber();
 
