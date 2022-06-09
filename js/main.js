73a581c4677a0e25bd7b8be5944272ba511848ba
Dmitrii Osadchenko <dimaos@protonmail.com>
2022-06-09 16:17:32 +0300

    Добавляет генерацию объявлений

--- js/main.js
+++ js/main.js
@@ -1,3 +1,32 @@
+const OFFER_TYPES = [
+  'palace',
+  'flat',
+  'house',
+  'bungalow',
+  'hotel',
+];
+
+const OFFER_TYME_VALUES = [
+  '12:00',
+  '13:00',
+  '14:00',
+];
+
+const OFFER_FEATURES = [
+  'wifi',
+  'dishwasher',
+  'parking',
+  'washer',
+  'elevator',
+  'conditioner',
+];
+
+const OFFER_PHOTOS = [
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jp',
+];
+
 const getRandomIntegerNumber = (from, to) => {
   if (from < 0) {
     throw new RangeError('Нижняя граница диаппазона не может быть отрицательной.');
@@ -33,5 +62,54 @@ const getRandomFloatNumber = (from, to, precision = 5) => {
   return Number((Math.random() * (to - from) + from).toFixed(precision));
 };
 
-getRandomIntegerNumber('55', 60);
-getRandomFloatNumber(2.25, 3.75, 2);
+const generateSubArray = (arr) => arr.filter(() => Math.random() < 0.5);
+
+const getFixLengthNumberString = (x, targetLength) => {
+  if (x.toString().length < targetLength) {
+    return `0${getFixLengthNumberString(x, targetLength - 1)}`;
+  }
+
+  return x;
+};
+
+const createOffer = () => {
+  const lat = getRandomFloatNumber(35.65, 35.7, 5);
+  const lng = getRandomFloatNumber(139.7, 139.8, 5);
+
+  return {
+    author: {
+      avatar: `img/avatars/user${getFixLengthNumberString(getRandomIntegerNumber(1, 10), 2)}.png`,
+    },
+    offer: {
+      title: 'Сдам',
+
+      address: `${lat}, ${lng}`,
+
+      price: getRandomIntegerNumber(1000, 100000),
+
+      type: OFFER_TYPES[getRandomIntegerNumber(0, OFFER_TYPES.length - 1)],
+
+      rooms: getRandomIntegerNumber(1, 10),
+
+      guests: getRandomIntegerNumber(1, 10),
+
+      checkin: OFFER_TYME_VALUES[getRandomIntegerNumber(0, OFFER_TYME_VALUES.length - 1)],
+
+      checkout: OFFER_TYME_VALUES[getRandomIntegerNumber(0, OFFER_TYME_VALUES.length - 1)],
+
+      features: generateSubArray(OFFER_FEATURES),
+
+      description: 'Здесь можно жить',
+
+      photos: generateSubArray(OFFER_PHOTOS),
+    },
+    location: {
+      lat,
+      lng,
+    },
+  };
+};
+
+const createOffers = Array.from({length: 10}, createOffer);
+
+export {createOffers};
