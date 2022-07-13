c76a3188454e2c28c934aea246ad153269d7e01b
Keks <keks@htmlacademy.ru>
2022-07-13 13:10:31 +0300

    :heavy_check_mark: Сборка #10

--- 10/js/modules/map.js
+++ 10/js/modules/map.js
@@ -11,7 +11,7 @@ const DEFAULT_MAP_COORDS = {
   lng: 139.75,
 };
 
-const DEFAULT_MAP_SCALE = 13;
+const DEFAULT_MAP_ZOOM = 13;
 
 const MAP_SETTINGS = {
   layer: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
@@ -35,8 +35,11 @@ const mapCanvas = document.querySelector('#map-canvas');
 const addressInput = document.querySelector('#address');
 
 let currentOffers;
+let map;
+let mainPinMarker;
+let pinIcon;
 
-const renderMarkers = (map, icon, offers) => {
+const renderMarkers = (icon, offers) => {
   offers.forEach((offer) => {
     const {
       location: {
@@ -50,7 +53,7 @@ const renderMarkers = (map, icon, offers) => {
         lng,
       },
       {
-        icon: icon,
+        icon,
       },
     );
 
@@ -61,11 +64,24 @@ const renderMarkers = (map, icon, offers) => {
 };
 
 const setDefaultAddress = () => {
-  addressInput.value = `${DEFAULT_MAP_COORDS.lat}, ${DEFAULT_MAP_COORDS.lng}`;
+  addressInput.value = `${DEFAULT_MAP_COORDS.lat.toFixed(5)}, ${DEFAULT_MAP_COORDS.lng.toFixed(5)}`;
+};
+
+const resetMap = () => {
+  if (map) {
+    map.closePopup();
+    map.setView(DEFAULT_MAP_COORDS);
+
+    setTimeout(() => {
+      map.setZoom(DEFAULT_MAP_ZOOM);
+    }, 300);
+  }
 };
 
 const resetMainPinMarker = () => {
-  window.mainPinMarker.setLatLng(DEFAULT_MAP_COORDS);
+  if (mainPinMarker) {
+    mainPinMarker.setLatLng(DEFAULT_MAP_COORDS);
+  }
 };
 
 const initMap = () => {
@@ -73,12 +89,23 @@ const initMap = () => {
     return;
   }
 
-  const map = L.map('map-canvas')
+  const onSuccessGetOffers = (offers) => {
+    currentOffers = offers;
+
+    renderMarkers(pinIcon, getRandomArrayElements(currentOffers, MAX_OFFERS));
+  };
+
+  const onFailGetOffers = (msg) => {
+    showAlert(msg);
+  };
+
+  map = L.map('map-canvas')
     .on('load', () => {
       activateMapFilters(true);
+      getData(onSuccessGetOffers, onFailGetOffers);
       activateAdFormElements(true);
     })
-    .setView(DEFAULT_MAP_COORDS, DEFAULT_MAP_SCALE);
+    .setView(DEFAULT_MAP_COORDS, DEFAULT_MAP_ZOOM);
 
   L.tileLayer(MAP_SETTINGS.layer, MAP_SETTINGS.attribution).addTo(map);
 
@@ -88,13 +115,13 @@ const initMap = () => {
     iconAnchor: [PIN.main.width / 2, PIN.main.height],
   });
 
-  const pinIcon = L.icon({
+  pinIcon = L.icon({
     iconUrl: './img/pin.svg',
     iconSize: [PIN.offer.width, PIN.offer.height],
     iconAnchor: [PIN.offer.width / 2, PIN.offer.height],
   });
 
-  const mainPinMarker = L.marker(
+  mainPinMarker = L.marker(
     DEFAULT_MAP_COORDS,
     {
       draggable: true,
@@ -102,30 +129,15 @@ const initMap = () => {
     },
   );
 
-  window.map = map;
-  window.mainPinMarker = mainPinMarker;
-
   setDefaultAddress(addressInput.value);
 
   mainPinMarker.addTo(map);
 
-  const onSuccessGetOffers = (offers) => {
-    currentOffers = offers;
-
-    renderMarkers(map, pinIcon, getRandomArrayElements(currentOffers, MAX_OFFERS));
-  };
-
-  const onFailGetOffers = (msg) => {
-    showAlert(msg);
-  };
-
-  getData(onSuccessGetOffers, onFailGetOffers);
-
   mainPinMarker.on('drag', ({ target }) => {
     const { lat, lng } = target.getLatLng();
 
-    addressInput.value = `${lat}, ${lng}`;
+    addressInput.value = `${Number(lat).toFixed(5)}, ${Number(lng).toFixed(5)}`;
   });
 };
 
-export { setDefaultAddress, resetMainPinMarker, initMap };
+export { setDefaultAddress, resetMap, resetMainPinMarker, initMap };
